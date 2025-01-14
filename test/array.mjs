0a24f9519e4ca25f26e358a081b8b328634d4ebf
hydroperfox <hydroperfox@gmail.com>
2025-01-14 09:17:36 -0300

    Update

--- test/array.mjs
+++ test/array.mjs
@@ -1,16 +1,16 @@
 import * as $ from "../src/index.js";
 
-const list = $.construct($.vectorfloatclass);
-console.log("const list = new <float>[];");
+const list = $.construct($.applytype($.arrayclass, [$.floatclass]));
+console.log("const list:[float] = [];");
 console.log("list.push(10.5) =", $.callproperty(list, null, "push", 10.5));
 console.log("list[0]++ =", $.postincrementproperty(list, null, 0));
 console.log("list[0] =", $.getproperty(list, null, 0));
 console.log("list.length =", $.getproperty(list, null, "length"));
 
-const listOfRegExp = $.construct($.applytype($.vectorclass, [$.regexpclass]));
-console.log("const listOfRegExp = new <RegExp>[];");
+const listOfRegExp = $.construct($.applytype($.arrayclass, [$.regexpclass]));
+console.log("const listOfRegExp:[RegExp] = [];");
 console.log("listOfRegExp.push(/(?:)/gi) =", $.callproperty(listOfRegExp, null, "push", $.construct($.regexpclass, "(?:)", "gi")));
 
-const dynamicList = $.construct($.applytype($.vectorclass, [null]));
-console.log("const dynamicList = new <*>[];");
+const dynamicList = $.construct($.applytype($.arrayclass, [null]));
+console.log("const dynamicList:[*] = [];");
 console.log("dynamicList.push(10.5) =", $.callproperty(dynamicList, null, "push", 10.5));
---
+++ test/map.mjs
@@ -0,0 +1,17 @@
+import * as $ from "../src/index.js";
+
+const map1 = $.construct($.applytype($.mapclass, [$.stringclass, $.stringclass]));
+console.log("const map1 = new Map.<*, *>();");
+console.log("map1.x = 'hi';");
+$.setproperty(map1, null, "x", "hi");
+console.log("map1.x ==", $.getproperty(map1, null, "x"));
+console.log("map1.length() ==", $.callproperty(map1, null, "length"));
+
+console.log("// Testing weak Map");
+const map2 = $.construct($.applytype($.mapclass, [$.regexpclass, $.floatclass]), true);
+console.log("const map2 = new Map.<RegExp, float>(true);");
+const regex = $.construct($.regexpclass, "(?:)", "gi");
+console.log("const regex = /(?:)/gi;");
+console.log("map2[regex] = 10;");
+$.setproperty(map2, null, regex, 10);
+console.log("map2[regex] ==", $.getproperty(map2, null, regex));
